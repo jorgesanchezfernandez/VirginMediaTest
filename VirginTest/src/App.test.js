f88d248f567abefc19b7822bc9c7aee883daedd5
Jorge Sanchez <xhercs@gmail.com>
2022-02-14 20:27:17 +0000

    Added new react test

--- VirginTest/src/App.test.js
+++ VirginTest/src/App.test.js
@@ -1,5 +1,34 @@
-﻿import { render, screen } from '@testing-library/react';
+﻿import React from "react";
+import { act } from "react-dom/test-utils";
+import { render, screen } from '@testing-library/react';
 import App from './App';
+
+
+it("renders scenario data", async () => {
+  const fakeScenarios = [{
+    scenarioID: 1,
+    userID: "userID",
+    surname: "fakeUSer",
+    forename: "fakeForename"
+  }];
+
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeScenarios)
+    })
+  );
+
+  await act(async () => {
+    render(<App />);
+  });
+
+  expect(document.getElementsByTagName("td")[0].textContent).toContain(fakeScenarios[0].userID);
+  expect(document.getElementsByTagName("td")[1].textContent).toContain(fakeScenarios[0].surname);
+  expect(document.getElementsByTagName("td")[2].textContent).toContain(fakeScenarios[0].forename);
+
+  // remove the mock to ensure tests are completely isolated
+  global.fetch.mockRestore();
+});
 
 test('scenario first test', () => {
     render(<App />);
